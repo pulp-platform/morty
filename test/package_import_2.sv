0857b631d8675c59ecd7368a30e943eebb377014
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2020-03-13 10:41:24 +0100

    Release v0.2.1

--- test/package_import_2.sv
+++ test/package_import_2.sv
@@ -6,6 +6,12 @@ module test import blub_pkg::*; #(
     /* Testonen */
     localparam blub_pkg::A C = 20;
     logic blub_pkg = blub_pkg::C;
+
+    always_comb begin : prepare_input
+        for (int unsigned i = 0; i < NUM_OPERANDS; i++) begin
+        local_operands[i] = operands_i[i] >> LANE*blub_pkg::fp_width(src_fmt_i);
+        end
+    end
 endmodule
 
 module test2;
