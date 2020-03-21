f43406f9faaa6ce4a0ec46dc7c011399a39d3015
Fabian Schuiki <fabian@schuiki.ch>
2020-03-21 18:02:09 +0100

    doc: Impl basic HTML rendering

--- test/doc.sv
+++ test/doc.sv
@@ -29,6 +29,13 @@ module fifo #(
     typedef word_t [3:0] qword_t;
 
     /// Some internal signals. Very strange.
+    ///
+    /// You are actually expected to use this as follows:
+    /// ```
+    /// always_ff @(posedge clk_i) begin
+    ///     data_q <= data_d;
+    /// end
+    /// ```
     word_t data_d, data_q;
 
     /// Magic nets.
